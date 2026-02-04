84d87ea2d3b7b4887d7b3655a54e7ff074fbf382
!skhak <nurlanjunushaliev76@icloud.com>
2026-02-04 19:08:20 +0600

    iskhak/regis-login

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { useRouter } from "next/navigation";
 import { IoIosArrowRoundForward } from "react-icons/io";
 
 const Header = () => {
-  return (
+  const router = useRouter();
+  let isAuth = false;
+  return isAuth ? (
     <header className="py-5">
       <div className="container">
         <div className="flex items-center justify-between gap-4">
@@ -14,7 +18,13 @@ const Header = () => {
           </nav>
           <div className="flex items-center gap-2.5">
             <button className="py-2.5 px-5 font-medium text-base">Войти</button>
-            <button className="button flex items-center gap-2.5">
+            <button
+              onClick={() => {
+                router.push("/auth/login");
+                isAuth = true;
+              }}
+              className="button flex items-center gap-2.5"
+            >
               Присоединяйся
               <IoIosArrowRoundForward className="text-xl" />
             </button>
@@ -22,6 +32,8 @@ const Header = () => {
         </div>
       </div>
     </header>
+  ) : (
+    ""
   );
 };
 
